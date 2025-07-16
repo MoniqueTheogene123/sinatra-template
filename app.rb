04870f9f6bde8ea15344caf1694132320643bc49
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-16 14:38:44 +0000

    password_generator_works

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "securerandom"
 
 
 get("/") do
+
+def secure_random_letter
+  SecureRandom.alphanumeric(10).tap do |char|
+    char =~ /[A-Za-z0-9]/ or return secure_random_letter
+  end
+end
+
 erb(:password_generator)
 end
