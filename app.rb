e04a8dfc5351fc3f272aa4afd838f47bba0c4ef8
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-18 15:12:21 +0000

    commit

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ get("/") do
 
 
 
-def secure_random_letter
-  SecureRandom.alphanumeric(10).tap do |char|
-    char =~ /[A-Za-z0-9]/ or return secure_random_letter
-  end
-end
+# def secure_random_letter
+#   SecureRandom.alphanumeric(10).tap do |char|
+#     char =~ /[A-Za-z0-9]/ or return secure_random_letter
+#   end
+# end
 
 erb(:password_generator)
 end
