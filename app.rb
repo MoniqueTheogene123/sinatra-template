190bfd867791e097756297abf2a672383b1bf04f
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-18 14:58:36 +0000

    setting_up_get

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require "sinatra"
 require "sinatra/reloader"
 require "securerandom"
-
+require "http"
 
 get("/") do
+@raw_response = HTTP.get("https://api.openbrewerydb.org/v1/breweries/random")
+
+@string_response = @raw_response.to_s
+
+@parsed_response = JSON.parse(@string_response)
+
+
 
 def secure_random_letter
   SecureRandom.alphanumeric(10).tap do |char|
